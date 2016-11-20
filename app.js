70b46d0a5f711b4b58f355c08b74ba5ce19c757a
Kenigbolo Meya Stephen <kenigbol@ut.ee>
2016-11-20 16:33:46 +0200

    Use two lines spacing for file

--- app.js
+++ app.js
@@ -2,25 +2,24 @@ var http = require('http')
 var server = http.createServer();
 
 server.on('request', function(request, response){
-	response.writeHead(200, {'Content-Type': 'application/json'});
-	var query = require('url').parse(request.url, true).path;
-
-	var apiResponse = http.get('http://api.fixer.io' + query, function(res){
-    var buffer = "",
-    		data;
+  response.writeHead(200, {'Content-Type': 'application/json'});
+  var query = require('url').parse(request.url, true).path;
+  var apiResponse = http.get('http://api.fixer.io' + query, function(res){
+    var buffer = "", data;
     res.on('data', function(chunk) {
- 			buffer += chunk;
-  	});
-  	res.on('end', function(){
-  		data = JSON.parse(buffer);
-  	});
-	});
+      buffer += chunk;
+    });
+    
+    res.on('end', function(){
+      data = JSON.parse(buffer);
+    });
+  });
 	
-	apiResponse.on("error", function(err){ 
+  apiResponse.on("error", function(err){ 
     console.error(err.message);
-	})
+  });
 
-	response.end();
+  response.end();
 });
 
 server.listen(8080);
