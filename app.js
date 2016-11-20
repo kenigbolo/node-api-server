f32e047afc794f47da00b7a7ec3997645db3a254
kenigbolo <kenigbol@ut.ee>
2016-11-20 21:05:49 +0200

    Refactor API to respond with proper json

--- app.js
+++ app.js
@@ -4,22 +4,17 @@ var server = http.createServer();
 server.on('request', function(request, response){
   response.writeHead(200, {'Content-Type': 'application/json'});
   var query = require('url').parse(request.url, true).path;
-  var apiResponse = http.get('http://api.fixer.io' + query, function(res){
-    var buffer = "", data;
+  http.get('http://api.fixer.io' + query, function(res){
+    var buffer = "";
     res.on('data', function(chunk) {
       buffer += chunk;
     });
-    
+
     res.on('end', function(){
-      data = JSON.parse(buffer);
+      response.write(buffer);
+      response.end();
     });
   });
-	
-  apiResponse.on("error", function(err){ 
-    console.error(err.message);
-  });
-
-  response.end();
 });
 
 server.listen(8080);
